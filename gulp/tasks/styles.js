6ebd84210b150f791d42b0cbce8b7fce4aeb36d7
alex-suschin <alex_suschin@mail.ru>
2021-01-16 11:11:35 +0300

    2

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -9,31 +9,27 @@ const gulpStylelint = require('gulp-stylelint')
 const rename = require("gulp-rename")
 
 module.exports = function styles() {
-  return gulp.src('src/styles/*.scss')
-    .pipe(plumber())
-    .pipe(gulpStylelint({
-      failAfterError: false,
-      reporters: [
-        {
-          formatter: 'string',
-          console: true
-        }
-      ]
-    }))
-    .pipe(sourcemaps.init())
-    .pipe(sass())
-    .pipe(autoprefixer({
-      cascade: false
-    }))
-    .pipe(shorthand())
-    .pipe(cleanCSS({
-      debug: true,
-      compatibility: '*'
-    }, details => {
-      console.log(`${details.name}: Original size:${details.stats.originalSize} - Minified size: ${details.stats.minifiedSize}`)
-    }))
-    .pipe(sourcemaps.write())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('build/css'))
+    return gulp.src('src/styles/*.scss')
+        .pipe(plumber())
+        .pipe(gulpStylelint({
+            failAfterError: false,
+            reporters: [{
+                formatter: 'string',
+                console: true
+            }]
+        }))
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(shorthand())
+        .pipe(cleanCSS({
+            debug: true,
+            compatibility: '*'
+        }, details => {
+            console.log(`${details.name}: Original size:${details.stats.originalSize} - Minified size: ${details.stats.minifiedSize}`)
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('build/css'))
 }
-
